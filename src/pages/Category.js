4bcb648060919aac914176ea033a617098fd57c0
Yos <yoselyncallejas19@gmail.com>
2025-06-19 18:28:07 +0200

    included image overlay to show amount of images within

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,6 +1,48 @@
+// import { useParams, Link } from "react-router-dom";
+// import projects from "../content/projects";
+// import { IoIosArrowBack } from "react-icons/io";
+// import "../styles/category.css";
+
+// function Category() {
+//   const { id } = useParams();
+//   const project = projects.find((proj) => proj.id === id);
+
+//   if (!project) return <p>Category not found.</p>;
+
+//   return (
+//     <div className="category-page">
+//       <header className="category-header">
+//         <Link to="/" className="back-button"> <IoIosArrowBack />
+//         Home</Link>
+//         <h1 className='project-title'>{project.title}</h1>
+//         <p className='project-description'>{project.description}</p>
+//       </header>
+//       <div className="subprojects-grid">
+//               { project.subprojects?.map((subproject) => (
+//             <div >
+//             <Link
+//                 key={subproject.id}
+//                 to={`/${id}/${subproject.id}`}
+//                 className="subproject-card"
+//             >
+//                 <img
+//                 src={subproject.mainImage}
+//                 alt={subproject.title}
+//                 className="subproject-image"
+//                 />
+//             </Link>
+//             <p>{ subproject.description }</p>
+//             </div>
+//         )) }
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Category;
+
 import { useParams, Link } from "react-router-dom";
 import projects from "../content/projects";
-import { IoIosArrowBack } from "react-icons/io";
 import "../styles/category.css";
 
 function Category() {
@@ -12,28 +54,34 @@ function Category() {
   return (
     <div className="category-page">
       <header className="category-header">
-        <Link to="/" className="back-button"> <IoIosArrowBack />
-        Home</Link>
+        <Link to="/" className="back-button">Back</Link>
         <h1 className='project-title'>{project.title}</h1>
         <p className='project-description'>{project.description}</p>
       </header>
       <div className="subprojects-grid">
-              { project.subprojects?.map((subproject) => (
-            <div >
-            <Link
-                key={subproject.id}
-                to={`/${id}/${subproject.id}`}
-                className="subproject-card"
-            >
-                <img
+        {project.subprojects?.map((subproject) => (
+        <div>
+          <Link
+            key={subproject.id}
+            to={`/${id}/${subproject.id}`}
+            className="subproject-card"
+          >
+            <div className="image-container">
+              <img
                 src={subproject.mainImage}
                 alt={subproject.title}
                 className="subproject-image"
-                />
+              />
+              <div className="image-overlay">
+                <span className="image-text">
+                  {subproject.images.length} Image{subproject.images.length > 1 ? "s" : ""}
+                </span>
+              </div>
+            </div>
             </Link>
             <p>{ subproject.description }</p>
-            </div>
-        )) }
+        </div>
+        ))}
       </div>
     </div>
   );
